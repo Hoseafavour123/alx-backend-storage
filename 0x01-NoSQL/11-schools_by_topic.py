03b7792e2ca24ab947ff3b8a95ad93aa7518bf0a
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-10-19 07:50:17 -0700

    Find schools

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""search schools by topics"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Search schools that match a topic"""
+    return mongo_collection.find("topics": {"$in": [topic]})
